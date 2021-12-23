d889b34e8dc6c2937cd646aefcf14bd385f7afe5
Murilo Rosa <mrmurilo75@gmail.com>
2021-12-23 01:49:42 +0000

    fix warning

--- src/up/project/chat/ChatServer.java
+++ src/up/project/chat/ChatServer.java
@@ -220,7 +220,7 @@ public class ChatServer
 
     private static void messageRoomExcept(byte[] msg, String forum, ClientInfo exc) {
         for( ClientInfo member : foruns.get(forum) ) {
-            if (exc != null && member.equals(exc)) continue;
+            if (member.equals(exc)) continue;
 
             messageClient(msg, member);
         }
